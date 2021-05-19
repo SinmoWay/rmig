b51fdb16d6d6039604a3445e15c1573a9581aeb1
sinmoway <sinmoway@gmail.com>
2021-05-20 00:30:49 +0300

    Add schema validation

--- .docker/oracle/build.bat
+++ .docker/oracle/build.bat
@@ -1,10 +1,11 @@
-@echo on
+@echo off
 set DIR=%~1
 
 rem See https://www.oracle.com/database/technologies/xe-downloads.html, if need another version.
 set ORACLE_FILE=oracle-database-xe-18c-1.0-1.x86_64.rpm
 set SINGLE_INSTANCE_REPO=https://github.com/oracle/docker-images/archive/refs/heads/main.zip
 set DEFAULT_ORACLE=https://edelivery.oracle.com/otn-pub/otn_software/db-express/%ORACLE_FILE%
+set GIT_SUB_DIR=OracleDatabase\SingleInstance\dockerfiles
 
 if "%DIR%"=="" ( set DIR=. )
 
@@ -18,11 +19,13 @@ if "%EX%" == "" (
     call wget.exe -v -P %DIR% %DEFAULT_ORACLE%
 )
 
-IF EXIST "%DIR%\docker-images-main/nul" (
-    echo Start building docker file.
-) else (
-    echo Download oracle docker-images (Git Repository)
-    rem call wget.exe -v -P %DIR% %SINGLE_INSTANCE_REPO%
+if NOT EXIST %DIR%\docker-images-main\ (
+    call wget.exe -v -P %DIR% %SINGLE_INSTANCE_REPO%
     echo Unzip files
-    rem call tar.exe -xf %DIR%\main.zip -C %DIR%\
+    call tar.exe -xf %DIR%\main.zip -C %DIR%\
+)
+
+IF EXIST %DIR%\docker-images-main\ (
+    echo Start building docker file.
+    call cp %DIR%
 )
